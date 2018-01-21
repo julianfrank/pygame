455f7fe24054d3e45cfc4a90bd432fb8e768358c
jf laptop ubuntu <jfempire@yahoo.co.uk>
2018-01-21 13:52:16 +0530

    banner game

--- t3.py
+++ t3.py
@@ -1,35 +1,41 @@
 """t3.py"""
-import pygame
+
 import sys
 
-screen = pygame.display.set_mode((640, 480))
-player = pygame.image.load('ball.bmp').convert()
-background = pygame.image.load('bg1600x1200.jpg').convert()
-screen.blit(background, (0, 0))
-objects = []
+import pygame
+
+SCREEN = pygame.display.set_mode((640, 480))
+PLAYER = pygame.image.load('ball.bmp').convert()
+BG = pygame.image.load('bg1600x1200.jpg').convert()
+SCREEN.blit(BG, (0, 0))
+OBJECTS = []
+
 
 class GameObject:
+    """GameObject"""
+
     def __init__(self, image, height, speed):
         self.speed = speed
         self.image = image
         self.pos = image.get_rect().move(0, height)
     def move(self):
+        """move"""
         self.pos = self.pos.move(self.speed, self.speed)
         if self.pos.right > 600:
             self.pos.left = 0
         if self.pos.bottom > 400:
             self.pos.top = 0
 
-for x in range(10):                    #create 10 objects</i>
-    o = GameObject(player, x*40, x)
-    objects.append(o)
+for x in range(10):                    #create 10 OBJECTS</i>
+    o = GameObject(PLAYER, x*40, x)
+    OBJECTS.append(o)
 while 1:
     for event in pygame.event.get():
         if event.type in (pygame.QUIT, pygame.KEYDOWN):
             sys.exit()
-    for o in objects:
-        screen.blit(background, o.pos, o.pos)
+    for o in OBJECTS:
+        SCREEN.blit(BG, o.pos, o.pos)
         o.move()
-        screen.blit(o.image, o.pos)
+        SCREEN.blit(o.image, o.pos)
     pygame.display.update()
     #pygame.time.delay(1)
